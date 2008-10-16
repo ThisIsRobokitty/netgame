2690ac7a1e47634ea58094dadb5446db27d25ee0
glenn.fiedler <glenn.fiedler@d6633234-9180-11dd-b9ac-2b9acee0b3cf>
2008-10-16 17:09:02 +0000

    go

--- Reliability and Flow Control/Reliability.cpp
+++ Reliability and Flow Control/Reliability.cpp
@@ -22,8 +22,119 @@ const float SendRate = 1.0f / 30.0f;
 const float TimeOut = 10.0f;
 const int PacketSize = 256;
 
+class FlowControl
+{
+public:
+	
+	FlowControl()
+	{
+		printf( "flow control initialized\n" );
+		Reset();
+	}
+	
+	void Reset()
+	{
+		mode = Bad;
+		penalty_time = 4.0f;
+		good_conditions_time = 0.0f;
+		penalty_reduction_accumulator = 0.0f;
+	}
+	
+	void Update( float deltaTime, float rtt )
+	{
+		const float RTT_Threshold = 250.0f;
+
+		if ( mode == Good )
+		{
+			if ( rtt > RTT_Threshold )
+			{
+				printf( "bad mode (rtt = %.2f)\n", rtt );
+				mode = Bad;
+				if ( good_conditions_time < 10.0f && penalty_time < 60.0f )
+				{
+					penalty_time *= 2.0f;
+					printf( "penalty time increased to %.1f\n", penalty_time );
+				}
+				good_conditions_time = 0.0f;
+				penalty_reduction_accumulator = 0.0f;
+				return;
+			}
+			
+			good_conditions_time += deltaTime;
+			penalty_reduction_accumulator += deltaTime;
+			
+			if ( penalty_reduction_accumulator > 10.0f && penalty_time > 1.0f )
+			{
+				penalty_time /= 2.0f;
+				printf( "penalty time reduced to %.1f\n", penalty_time );
+				penalty_reduction_accumulator = 0.0f;
+			}
+		}
+		
+		if ( mode == Bad )
+		{
+			if ( rtt <= RTT_Threshold )
+				good_conditions_time += deltaTime;
+			else
+				good_conditions_time = 0.0f;
+				
+			if ( good_conditions_time > penalty_time )
+			{
+				printf( "good mode (rtt = %.2f)\n", rtt );
+				good_conditions_time = 0.0f;
+				penalty_reduction_accumulator = 0.0f;
+				mode = Good;
+				return;
+			}
+		}
+	}
+	
+	float GetSendRate()
+	{
+		return mode == Good ? 30.0f : 10.0f;
+	}
+	
+private:
+
+	enum Mode
+	{
+		Good,
+		Bad
+	};
+
+	Mode mode;
+	float penalty_time;
+	float good_conditions_time;
+	float penalty_reduction_accumulator;
+};
+
+// ----------------------------------------------
+
 int main( int argc, char * argv[] )
 {
+	// parse command line
+
+	enum Mode
+	{
+		Client,
+		Server
+	};
+
+	Mode mode = Server;
+	Address address;
+
+	if ( argc >= 2 )
+	{
+		int a,b,c,d;
+		if ( sscanf( argv[1], "%d.%d.%d.%d", &a, &b, &c, &d ) )
+		{
+			mode = Client;
+			address = Address(a,b,c,d,ServerPort);
+		}
+	}
+
+	// initialize
+
 	if ( !InitializeSockets() )
 	{
 		printf( "failed to initialize sockets\n" );
@@ -32,19 +143,38 @@ int main( int argc, char * argv[] )
 
 	ReliableConnection connection( ProtocolId, TimeOut );
 	
-	if ( !connection.Start( ClientPort ) )
+	const int port = mode == Server ? ServerPort : ClientPort;
+	
+	if ( !connection.Start( port ) )
 	{
-		printf( "could not start connection on port %d\n", ClientPort );
+		printf( "could not start connection on port %d\n", port );
 		return 1;
 	}
 	
-	connection.Connect( Address(127,0,0,1,ServerPort ) );
+	if ( mode == Client )
+		connection.Connect( address );
+	else
+		connection.Listen();
 	
 	bool connected = false;
 	float accumulator = 0.0f;
 	
+	FlowControl flowControl;
+
+	bool connected = false;
+	float sendAccumulator = 0.0f;
+	
 	while ( true )
 	{
+		// update flow control
+		
+		if ( connection.IsConnected() )
+			flowControl.Update( DeltaTime, connection.GetReliabilitySystem().GetRoundTripTime() * 1000.0f );
+		
+		const float sendRate = flowControl.GetSendRate();
+
+		// detect changes in connection state
+
 		if ( !connected && connection.IsConnected() )
 		{
 			printf( "client connected to server\n" );
@@ -57,9 +187,17 @@ int main( int argc, char * argv[] )
 			break;
 		}
 		
-		unsigned char packet[PacketSize];
+		// send and receive packets
 		
-		connection.SendPacket( packet, sizeof( packet ) );
+		sendAccumulator += DeltaTime;
+		
+		while ( sendAccumulator > 1.0f / sendRate )
+		{
+			unsigned char packet[PacketSize];
+			memset( packet, 0, sizeof( packet ) );
+			connection.SendPacket( packet, sizeof( packet ) );
+			sendAccumulator -= 1.0f / sendRate;
+		}
 		
 		while ( true )
 		{
@@ -69,9 +207,15 @@ int main( int argc, char * argv[] )
 				break;
 		}
 		
+		// process acks
+		
+		// ...
+
+		// update connection
+		
 		connection.Update( DeltaTime );
 
-		// show stats
+		// show connection stats
 		
 		accumulator += DeltaTime;
 
